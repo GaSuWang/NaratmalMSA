5455011a51ade377aef3cf7d2077afed17840a3f
GaSuWang <kws04254@naver.com>
2023-02-17 16:46:48 +0900

    construct Injection

--- user/src/main/java/com/naratmal/user/controller/Controller.java
+++ user/src/main/java/com/naratmal/user/controller/Controller.java
@@ -4,6 +4,7 @@ package com.naratmal.user.controller;
 import com.naratmal.user.dto.RegistUserReq;
 import com.naratmal.user.dto.UserLoginRes;
 import com.naratmal.user.service.UserService;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +15,16 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
+@AllArgsConstructor
 public class Controller {
     private Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    @Autowired
+
     UserService userService;
 
     @GetMapping("/checknickname/{nickname}")
     public ResponseEntity<Boolean> checkNickname(@PathVariable String nickname){
+        logger.info("[CheckNickname] Parameter {}",nickname);
         return ResponseEntity.ok(userService.checkNickname(nickname));
     }
 
@@ -34,7 +37,7 @@ public class Controller {
 
     @GetMapping("/{code}")
     public ResponseEntity<UserLoginRes> login(@PathVariable String code){
-        logger.info("[Regist User] Parameter {}",code);
+        logger.info("[login User] Parameter {}",code);
         UserLoginRes res = null;
         return ResponseEntity.status(200).body(res);
     }
