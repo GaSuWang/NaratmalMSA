1c32c5a4465482db77074e6efda38b59b8d5ec8b
GaSuWang <kws04254@naver.com>
2023-04-11 09:39:41 +0900

    delete 수정

--- note/src/api/DeleteNote.go
+++ note/src/api/DeleteNote.go
@@ -13,8 +13,12 @@ func DeleteNote(context *gin.Context) {
 	deleteSeq := deleteNoteReq{}
 	context.BindJSON(deleteSeq)
 	conn := DB.GetInstance()
-
+	var note DB.Note
 	//1.delete seq, 작성자같은지 확인
-	//2.같으면 삭제 다르면 에러
+	conn.Where("note_seq = ?", deleteSeq.noteSeq).Find(&note)
+	if note.UserEmail != context.GetHeader("Authorization-Email") {
+		return
+	}
+	//2.같으면 삭제
 	conn.Delete(&DB.Note{}, deleteSeq.noteSeq)
 }
